8510da80974fedb6be5faebce2771b766be245ab
Andy Towers <atowers23@gmail.com>
2017-09-24 13:55:57 -0500

    Final elegant revision

--- program2.sh
+++ program2.sh
@@ -6,12 +6,11 @@
 # been added since the last output will be printed to the screen
 
 
-ls -lA | grep -i "^[^dt]..*\.pdf$" | sed 's/  */@/g' | cut -d@ -f9 >/tmp/.lastcheck # Initialize contents of file .lastcheck with current .pdf files in directory
+ls -FA | grep -i "\.pdf$" | sort >/tmp/.lastcheck # Initialize contents of file temporary file .lastcheck with sorted list of current .pdf files in directory
 while true # Infinite Loop
 do
-    sleep 3 # Wait 60 seconds
-    ls -lA | grep -i "^[^dt]..*\.pdf$" | sed 's/  */@/g' | cut -d@ -f9 >/tmp/.sincesleep # Check directory for .pdf files and save them into file .sincesleep
-    #sort /tmp/.lastcheck -o /tmp/.lastcheck ; sort /tmp/.sincesleep -o /tmp/.sincesleep # Sort files before using comm 
+    sleep 60 # Wait 60 seconds
+    ls -FA | grep -i "\.pdf$" | sort >/tmp/.sincesleep # Check directory for .pdf files and save sorted output into temporary file .sincesleep
     comm -23 /tmp/.sincesleep /tmp/.lastcheck  # Compare contents of current directory and directory 60 seconds ago, print only files that have been added
-    cp /tmp/.sincesleep /tmp/.lastcheck # Set  .lastcheck to contents of .sincesleep so comparison will always be done with last read
+    cp /tmp/.sincesleep /tmp/.lastcheck # Set .lastcheck to contents of .sincesleep so comparison will always be done with last read
 done
