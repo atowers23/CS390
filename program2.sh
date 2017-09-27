2298d7267584ba1c7f0bf06928731dde76a40b0c
Andy Towers <atowers23@gmail.com>
2017-09-26 22:00:34 -0500

    File must have character before .pdf

--- program2.sh
+++ program2.sh
@@ -6,11 +6,11 @@
 # been added since the last output will be printed to the screen
 
 
-ls -FA | grep -i "\.pdf$" | sort >/tmp/.lastcheck # Initialize contents of file temporary file .lastcheck with sorted list of current .pdf files in directory
+ls -FA | grep -i ".\.pdf$" | sort >/tmp/.lastcheck # Initialize contents of file temporary file .lastcheck with sorted list of current .pdf files in directory
 while true # Infinite Loop
 do
     sleep 60 # Wait 60 seconds
-    ls -FA | grep -i "\.pdf$" | sort >/tmp/.sincesleep # Check directory for .pdf files and save sorted output into temporary file .sincesleep
+    ls -FA | grep -i ".\.pdf$" | sort >/tmp/.sincesleep # Check directory for .pdf files and save sorted output into temporary file .sincesleep
     comm -23 /tmp/.sincesleep /tmp/.lastcheck  # Compare contents of current directory and directory 60 seconds ago, print only files that have been added
     cp /tmp/.sincesleep /tmp/.lastcheck # Set .lastcheck to contents of .sincesleep so comparison will always be done with last read
 done
