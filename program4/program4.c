edaa2ba309561c87bb62b42e719722724e18a2a2
Andy Towers <atowers23@gmail.com>
2017-10-24 19:41:05 -0500

    Closer...

--- program4/program4.c
+++ program4/program4.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /*
 INSERT BLOCK COMMENT DESCRIBING PROGRAM
@@ -11,22 +13,21 @@ INSERT BLOCK COMMENT DESCRIBING PROGRAM
 
 int main(int argc, char **argv)
 {
-	char directory_path[300];
 	char start_directory[500];
 	char file_name[256];
+	char write_buf[256];
 	DIR *dir;
 	struct dirent *dir_entry;
 	struct stat stat_buf_send;
 	struct stat stat_buf_receive;
-	mode_t file_type;
-	off_t file_size;
-	blkcnt_t num_blocks;
+	int fd_dir_stats;
 	int regular_file_count = 0;
 	int directory_count = 0;
 	int total_filesize = 0;
 	int block_count = 0;
 	int member_count = 0;
 	int the_pipe[2];
+	int status;
 
 	if (argc == 1)
 	{
@@ -34,70 +35,107 @@ int main(int argc, char **argv)
 		exit(-1);
 	}
 
-	strcpy(directory_path, argv[1]);
-	if ((getcwd(start_directory, 500)) == -1)
+	if ((getcwd(start_directory, 500)) == NULL) //store the starting directory to return to it at program close
 	{
 		perror("getcwd");
 		exit(-1);
 	}
-	if ((chdir(directory_path)) == -1)
+	if ((chdir(argv[1])) == -1) // change directories to target directory
 	{
 		perror("chdir");
 		exit(-1);
 	}
-	dir = opendir(directory_path);
-	chdir(directory_path);
+	if ((dir = opendir(argv[1])) == NULL) //open target directory
+	{
+		perror("opendir");
+		exit(-1);
+	}
 
 	readdir(dir); // Skip current directory
 	readdir(dir); // Skip parent directory
+
 	if ((pipe(the_pipe)) == -1)
 	{
 		perror("pipe");
 		exit(-1);
 	}
-	while ((dir_entry = readdir(dir)) != NULL)
+
+	if((fd_dir_stats = open("directory_stats", O_RDWR | O_CREAT | O_TRUNC)) == -1)
 	{
-		member_count+=1;
-		lstat(dir_entry->d_name, &stat_buf_send);
-		if ((write(the_pipe[1], &stat_buf_send, sizeof(stat_buf_send))) == -1)
-		{
-			perror("write");
-			exit(-1);
-		}
-		if ((read(the_pipe[0], &stat_buf_receive, sizeof(stat_buf_receive))) == -1)
-		{
-			perror("write");
-			exit(-1);
-		}
-		if (S_ISREG(stat_buf_receive.st_mode))
-		{
-			regular_file_count += 1;
-		}
-		else if (S_ISDIR(stat_buf_receive.st_mode))
-		{
-			directory_count += 1;
-		}
-
-		total_filesize += (int)stat_buf_receive.st_size;
-
-		block_count += (int)stat_buf_receive.st_blocks;
-		
-
-		strcpy(directory_path, argv[1]);
-	}
-	
-	fprintf(stdout, "DIRECTORY STATS\n");
-	fprintf(stdout, "------------------------------\n");
-	fprintf(stdout, "Regular files:%40d\n", regular_file_count);
-	fprintf(stdout, "Directories:%42d\n", directory_count);
-	fprintf(stdout, "Total filesize (bytes):%31d\n", total_filesize);
-	fprintf(stdout, "Total storage allocation (blocks):%22d\n", block_count);
-	fprintf(stdout, "Members(debug):%42d\n", member_count);
-	
-	
-	if ((chdir(start_directory)) == -1)
-	{
-		perror("chdir");
+		perror("open");
 		exit(-1);
 	}
-}
+
+	switch (fork())
+	{
+		case 0:
+			close(the_pipe[1]);
+			stat_buf_receive.st_ino = -1;
+			while(stat_buf_receive.st_ino != 0)
+			{
+				if ((read(the_pipe[0], &stat_buf_receive, sizeof(stat_buf_receive))) == -1)
+			{
+				perror("write");
+				exit(-1);
+			}
+			if (S_ISREG(stat_buf_receive.st_mode))
+			{
+				regular_file_count += 1;
+			}
+			else if (S_ISDIR(stat_buf_receive.st_mode))
+			{
+				directory_count += 1;
+			}
+
+			total_filesize += (int)stat_buf_receive.st_size;
+
+			block_count += (int)stat_buf_receive.st_blocks;
+			}
+
+			sprintf(write_buf, "DIRECTORY STATS\n");
+			write(fd_dir_stats, write_buf, strlen(write_buf));
+			sprintf(write_buf, "------------------------------\n");
+			write(fd_dir_stats, write_buf, strlen(write_buf));
+			sprintf(write_buf, "Regular files:%40d\n", regular_file_count);
+			write(fd_dir_stats, write_buf, strlen(write_buf));
+			sprintf(write_buf, "Directories:%42d\n", directory_count);
+			write(fd_dir_stats, write_buf, strlen(write_buf));
+			sprintf(write_buf, "Total filesize (bytes):%31d\n", total_filesize);
+			write(fd_dir_stats, write_buf, strlen(write_buf));
+			sprintf(write_buf, "Total storage allocation (blocks):%22d\n", block_count);
+			write(fd_dir_stats, write_buf, strlen(write_buf));
+
+			exit(0);
+				
+		default:
+			close(the_pipe[0]);
+			while ((dir_entry = readdir(dir)) != NULL)
+			{
+				member_count+=1;
+				lstat(dir_entry->d_name, &stat_buf_send);
+				if ((write(the_pipe[1], &stat_buf_send, sizeof(stat_buf_send))) == -1)
+				{
+					perror("write");
+					exit(-1);
+				}
+			}
+
+			stat_buf_send.st_ino = 0;
+			if ((write(the_pipe[1], &stat_buf_send, sizeof(stat_buf_send))) == -1)
+			{
+				perror("write");
+				exit(-1);
+			}
+
+			wait(&status);
+
+			if ((chdir(start_directory)) == -1)
+			{
+				perror("chdir");
+				exit(-1);
+			}
+			close(fd_dir_stats);
+			exit(0);
+		
+	}
+}	
